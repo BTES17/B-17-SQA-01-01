a08a4f1b10e8fe978ad6d828a76e880a5f1c9ffe
BTES17 <ramandeep@btes.co.in>
2017-03-22 22:10:31 +0530

    vishali

---
+++ pyramid.java
@@ -0,0 +1,24 @@
+package vishali;
+
+public class pyramid {
+    public static void main(String args[])
+    {
+    	for (int i = 1;i <=5; i++)
+    	{ 
+    		for (int j = 1;j <=5 ; j++) 
+    		{
+    			System.out.print(" ");
+    			} 
+    		for (int k = 1;k <= i; k++)
+    		{
+    			System.out.print(k);
+    			} 
+    	//ystem.out.println();
+    		}
+    	
+    	
+    	}
+
+    	
+	
+}
