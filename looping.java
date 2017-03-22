bbee21c0374ab1b7da0583862abb5cb6b3b2d1e1
BTES17 <ramandeep@btes.co.in>
2017-03-22 18:39:36 +0530

    Anusha
    Greatest of 3 no.s

---
+++ looping.java
@@ -0,0 +1,23 @@
+
+public class looping 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=5,b=9,c=6;
+		if(a>b && a>c)
+		{
+			System.out.println("A is greatest");
+			
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("B is greatest");
+		}
+		else
+		{
+			System.out.println("C is greatest");
+		}
+	}
+
+}
