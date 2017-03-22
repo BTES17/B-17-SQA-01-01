8b5f727c032c3e23e468acb8d0c0496dfbcd0f96
BTES17 <ramandeep@btes.co.in>
2017-03-22 20:17:33 +0530

    vishali

---
+++ ifelse.java
@@ -0,0 +1,21 @@
+package vishali;
+
+public class ifelse {
+	public static void main(String args[])
+	{
+		int a,b,c;
+		//System.out.println("Enter three integers ");
+	      //Scanner in = new Scanner(System.in);
+	      a= 61;
+	      b = 9;
+	      c = 1;
+	        if (a>b && a>c)
+	    	    System.out.println("first number is greatest");  
+	    	else if (b>a && b>c)
+	    		System.out.println("second number is greatest");
+	    	else if (c>a && c>b)
+	    		System.out.println("third number is greatest");
+	
+	}
+
+}
