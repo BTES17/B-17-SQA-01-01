0b67fea3cbb41b0dbd6642c8b387c599d48781c7
BTES17 <ramandeep@btes.co.in>
2017-03-22 11:27:08 +0530

    Ankita
    WAP to find greatest of three numbers

--- greatest.java
+++ greatest.java
@@ -1,27 +1,23 @@
-package ranag;
 
 public class greatest {
-
-	public static void main(String[] args){
-		
-		greatest();
+	static public void main(String args[])
+	{
+		greatest1();
 	}
-static void greatest(){
-	
-	int x=9,y=8,z=7;
+static void greatest1()
+{
+	int x=9,y=8,z=10;
 	if((x>y)&&(x>z))
 	{
 		System.out.println(x);
-		
 	}
-	else if((y>z)&&(y>x))
+	else if((y>x)&&(y>z))
 	{
 		System.out.println(y);
 	}
-	else if((z>x)&&(z>y))
+	else if(( z>x)&&(z>y))
 	{
 		System.out.println(z);
 	}
-	}
-	
+}
 }
