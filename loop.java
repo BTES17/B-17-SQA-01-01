1beef1fcc27fe375319b0f7782a2471ba9dfac67
BTES17 <ramandeep@btes.co.in>
2017-03-23 10:21:17 +0530

    Shilpa modi
    wap to print greatest among three numbers

---
+++ loop.java
@@ -0,0 +1,26 @@
+package shy;
+
+public class loop {
+
+ public static void main(String[]args) {
+	int a,b,c;
+	a=1000;
+	b=900;
+	c=100;
+	
+	if(a>=b && a>=c)
+	{
+	 System.out.println("first no. is largest ");
+	}
+	else if (b>=a && b>=c)
+	{
+		System.out.println("second no. is greatest");
+	}
+	else 
+	{
+		System.out.println("third no is greatest");
+	}
+ }
+}
+	
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
