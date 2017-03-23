9c5154417cd9ea4eb847047823276b710fa5fa7e
BTES17 <ramandeep@btes.co.in>
2017-03-23 10:30:06 +0530

    Sonia Joshi
    WAP to find greatest among 3 numbers.

---
+++ loops.java
@@ -0,0 +1,29 @@
+package soniyajoshi;
+
+public class loops {
+
+	  static public void main(String[] args)
+	     { 
+		 
+	    	 int a=10;
+	    	 int b=100;
+	    	 int c=1000;
+	    	 if(a>b && a>c )
+	    	 {
+	    	 System.out.print(a);
+	    	 }
+	    	 else if(b>c && b>a)
+	    	 {
+	    	 System.out.println(b);
+	    	 }
+	    	 
+	    	 else
+	    	 {
+	    		 System.out.println(c);
+	    	
+	    	 
+	    	 
+	    	 
+	     }}
+	     
+}
