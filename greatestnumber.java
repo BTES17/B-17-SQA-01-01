87b5ad3f99cbbae0ed555f11f675339acbb2e2a9
BTES17 <ramandeep@btes.co.in>
2017-03-23 17:04:31 +0530

    Harjeet

---
+++ greatestnumber.java
@@ -0,0 +1,29 @@
+
+public class greatestnumber 
+{
+	public static void main(String args[])
+	{
+		 ifelse();
+	}
+
+	static void ifelse()
+	{
+		int a=20;
+	    int b=15;
+		int c=10;
+		if (a>b&&a>c)
+		{
+			System.out.println("a is greatest");
+		}
+		else if(b>a&&b>c)
+		{
+			System.out.println("b is greatest");
+		}
+		else
+		{
+			System.out.println("c is greatest");
+		}
+		
+		
+	}
+}
