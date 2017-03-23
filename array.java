c0403ef7b4a26c1c66e8c2dda7f9d8b37efe2c31
BTES17 <ramandeep@btes.co.in>
2017-03-23 17:57:40 +0530

    vishali

---
+++ array.java
@@ -0,0 +1,23 @@
+package vishali;
+
+import java.util.Scanner;
+
+public class array {
+public static void main(String args[]) 
+{
+	Scanner scan= new Scanner(System.in);
+	int a,i;
+	int[] arr = new int[10];
+	//Scanner scan= new Scanner(System.in);
+	System.out.println("enter the values");
+	//a= scan.nextInt();
+	//System.out.println(a);
+	for(i=0;i<10;i++)
+	{
+		//System.out.println("enter the values");
+		a= scan.nextInt();
+		//System.out.println(arr[i]);   
+		System.out.println(a);
+	}
+}
+}
