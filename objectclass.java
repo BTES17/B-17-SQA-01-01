fc318ae25f727294de2cdda5def9c30b740a3fdb
BTES17 <ramandeep@btes.co.in>
2017-03-24 11:41:23 +0530

    Harjeet

---
+++ objectclass.java
@@ -0,0 +1,20 @@
+
+public class objectclass {
+	public static void main(String[] args)
+	{
+	int a=20;
+	int b=30;
+	int c= a+b;
+	System.out.println(c);
+	objectclass object=new objectclass();
+		System.out.println(object. method1());
+		}
+	boolean method1()
+	{
+		int a=6,b=3;
+		boolean c = a>b;
+		System.out.println(c);
+		return c;
+	}
+
+}
