880330c186eaf331f6d1711ae156de775931d981
BTES17 <ramandeep@btes.co.in>
2017-03-24 11:48:55 +0530

    Ravdeep

---
+++ call.java
@@ -0,0 +1,24 @@
+
+public class rav {
+	public int add()
+{int a=4;
+ int b=6;
+return a+b;	
+}
+public int subtract()
+{
+	int c=7;
+	int d=4;
+return c-d;	
+}
+public static void main(String args[])
+{
+	rav obj=new rav();
+int f= obj.add();
+System.out.println(f);
+int g=obj.subtract();
+System.out.println(g);
+	
+}  }
+
+		
