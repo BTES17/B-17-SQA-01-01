84947efd9b7a2a2e21326ebe0463f0e419db823a
BTES17 <ramandeep@btes.co.in>
2017-03-24 11:05:58 +0530

    sheena

---
+++ sheena.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+	
+	public class abcd
+	{
+		public static void main(String args[]) 
+		{
+			Scanner scan= new Scanner(System.in);
+			int a,i;
+			int[] arr = new int[10];
+			
+			System.out.println("Enter the values");
+			
+			
+			for(i=0;i<10;i++)
+			{
+				
+				arr[i]= scan.nextInt();
+				System.out.println(arr[i]);   
+		
+			}}}
+
