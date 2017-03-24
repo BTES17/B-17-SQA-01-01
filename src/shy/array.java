50b47d69c95825f59594d2fde2460a9c2cf4fdb3
BTES17 <ramandeep@btes.co.in>
2017-03-24 10:29:52 +0530

    shilpa
    program of array

---
+++ src/shy/array.java
@@ -0,0 +1,24 @@
+package shy;
+import java.util.Scanner;
+public class array {
+
+	public array() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int i;
+		int[] arr=new int[10];
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter values");
+		for(i=0;i<10;i++)
+		{
+			arr[i]=scan.nextInt();
+			System.out.println(new int[10]);
+		}
+System.out.println(arr[1]);
+	}
+
+}
