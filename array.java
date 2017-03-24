4c9cb9443489463782ba4113a002b2097471bf74
BTES17 <ramandeep@btes.co.in>
2017-03-24 10:49:18 +0530

    Harjeet

--- array.java
+++ array.java
@@ -1,23 +1,21 @@
-package vishali;
-
 import java.util.Scanner;
 
 public class array {
-public static void main(String args[]) 
+public static void main(String[] args)
 {
-	Scanner scan= new Scanner(System.in);
-	int a,i;
-	int[] arr = new int[10];
-	//Scanner scan= new Scanner(System.in);
-	System.out.println("enter the values");
-	//a= scan.nextInt();
-	//System.out.println(a);
-	for(i=0;i<10;i++)
+	int[] array=new int[10];
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter a value");
+	for(int i=0; i<=9;i++)
 	{
-		//System.out.println("enter the values");
-		a= scan.nextInt();
-		//System.out.println(arr[i]);   
-		System.out.println(a);
+		array [i]=scan.nextInt();
+		
 	}
+	for(int i=0; i<=9;i++)
+	{
+		
+		System.out.println(array[i]);
+	}
+			
 }
 }
