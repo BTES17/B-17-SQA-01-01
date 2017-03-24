e0311e8433c94cf9f5e7abc46ee296e7449c0ea5
BTES17 <ramandeep@btes.co.in>
2017-03-24 11:24:38 +0530

    sheena

---
+++ object.java
@@ -0,0 +1,33 @@
+package sheena;
+
+public class object {
+
+	public object()
+	{
+	}
+	public static void main(String args[])
+{
+	int a=45;
+	int b=76;
+	if(a>b)
+	{
+		System.out.println("A is greater");
+	}
+	else
+	{	System.out.println("B is greater");
+	
+}
+	object object=new object();
+
+System.out.println(object.object2());
+}
+int object2()
+{
+	int a=18;
+	int b=7;
+	int c;
+	c=a*b;
+	return c;
+}
+}
+
