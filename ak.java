e113a8de4953be731ba6de7ed1ad36c1efb75361
BTES17 <ramandeep@btes.co.in>
2017-03-23 23:38:58 -0700

    SUNIL
    Wap to store the values in arrays using scanner class.

---
+++ ak.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+//import java.lang.*;
+
+
+public class ak {
+
+	public static void main(String args[]) {
+		
+		Scanner s=new Scanner(System.in);
+		 int[] number=new int[5];
+
+		 System.out.println("enter the value");
+		 
+		 for(int i=0;i<5;i++)
+		 {
+			 number[i]=s.nextInt();
+			 
+			 System.out.println(i);
+				 
+				 
+				 
+				 
+				 
+			  
+			 
+			 
+			 
+		 
+		 }	  
+		}
+		 
+		 }
+		 
+		
+		 
+				 
+
