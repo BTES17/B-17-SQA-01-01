84188b07af7162709b476fbdcdf601ba9db1f4c6
BTES17 <ramandeep@btes.co.in>
2017-03-26 15:01:01 +0530

    jashan

---
+++ array2.java
@@ -0,0 +1,24 @@
+package array;
+
+public class array2 {
+	public static void main(String[] args){
+	 int a=10;
+	 int b=20;
+	 int c=a+b;
+	 System.out.println(c);
+	 array2 object=new array2();
+	 System.out.println(object.method1());
+		 
+	}
+		int method1()
+		{
+			int a=7,b=5;
+			int c=a+b;
+			System.out.println(c);
+			return c;
+		}
+		
+		
+	}
+	
+
