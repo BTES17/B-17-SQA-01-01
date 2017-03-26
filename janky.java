0dfee2bf49509465f49c81972f4630f4155cd5d3
BTES17 <ramandeep@btes.co.in>
2017-03-26 21:58:04 +0530

    janky

---
+++ janky.java
@@ -0,0 +1,24 @@
+package abc;
+
+public class janky {
+static new1 obj1 =new new1();
+	public static void main(String[] args) {
+	
+	int a,b,c;
+	a=10;
+	b=10;
+	c=a+b;
+	System.out.println(c);
+	obj1.method1();
+	}
+	}
+class new1{
+
+ int method1 ()
+ {
+	int a=1,b=2,c;
+	c=a*b;
+	System.out.println(c);
+	return 0;
+}	
+}
