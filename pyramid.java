3aa4fee67de8ab51cf421eb63218029607a149dc
BTES17 <ramandeep@btes.co.in>
2017-03-26 15:32:30 +0530

    Shilpa
    pyramid

--- pyramid.java
+++ pyramid.java
@@ -1,24 +1,18 @@
-package vishali;
+package shilpa;
 
 public class pyramid {
-    public static void main(String args[])
-    {
-    	for (int i = 1;i <=5; i++)
-    	{ 
-    		for (int j = 1;j <=5 ; j++) 
-    		{
-    			System.out.print(" ");
-    			} 
-    		for (int k = 1;k <= i; k++)
-    		{
-    			System.out.print(k);
-    			} 
-    	//ystem.out.println();
-    		}
-    	
-    	
-    	}
 
-    	
-	
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++)
+		{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print(j);
+		}
+		System.out.print("\n");
+	}
+	}
 }
+
+
