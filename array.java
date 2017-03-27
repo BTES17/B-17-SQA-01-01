03092ea8848c2e6ec0a6f2e1c9cb422008da7c11
BTES17 <ramandeep@btes.co.in>
2017-03-27 11:24:52 +0530

    sheena
    highest value in array

--- array.java
+++ array.java
@@ -1,23 +1,24 @@
-package array;
-
 import java.util.Scanner;
-
 public class array {
-
-	public static void main(String[] args) {
-		int [] array=new int[10];
-		Scanner scan=new Scanner(System.in);
-		System.out.println("enter a value");
-		for(int i=0;i<=9;i++)
-		{
-		array [i]=scan.nextInt();
-			
-		}
-		for(int i=0;i<=9;i++)
-		{
-			System.out.println(array[i]);
-		}
-
+public static void main(String args[])
+{
+	int i;
+	Scanner scan =new Scanner(System.in);
+	int arr1[]=new int[10];
+	System.out.println("enter the values of array");
+	for(i=0;i<=9;i++)
+	{arr1[i]=scan.nextInt();
 	}
-
-}
+	for(i=0;i<=9;i++)
+	{System.out.println(arr1[i]);
+	}
+	int max=arr1[0];
+	for(i=0;i<=9;i++)
+	{if(max<arr1[i])
+	{
+		max=arr1[i];
+		i++;
+	}
+	System.out.println( "highest value of array is"+max);
+	
+	}}}
