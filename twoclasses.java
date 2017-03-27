75c1c6b544c313025cd87d16fa031d2655b50b1f
BTES17 <ramandeep@btes.co.in>
2017-03-27 17:18:51 +0530

    Harjeet

--- twoclasses.java
+++ twoclasses.java
@@ -1,24 +1,35 @@
-package vehicle;
 
-public class twoclasses 
-{
-	static twoclasses1 obj= new twoclasses1();
-	public static void main(String[] args)
-	{
-		int a=4,b=6;
-		System.out.println(a + "" + b);
-		obj.display();
+public class twoclasses {
+
+	static class2 object= new class2();
+			
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println(a+""+b);
+		object.display();
+		
+
+
 	}
-
 }
-class twoclasses1
+
+
+
+class class2
 {
-	int a=9;
-
-int display()
-{
-	System.out.println(a);
-	return 0;
+	int display()
+	{
+		int a=30;
+		System.out.println(a);
+	
+		return 0;	
+	
+	
 }
-}
 
+}
+	
+	
+	
+	
