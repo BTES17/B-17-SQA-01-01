c88427d4907247d45692b6035a11bbbb8f901aa7
BTES17 <ramandeep@btes.co.in>
2017-03-26 23:12:05 -0700

    Sunil 1
    Create an method Myarray() that take arry as input parameter, MyArray () increase the value of each element of the array to 3, return the updated value and print the update array in the main method

--- ak.java
+++ ak.java
@@ -4,32 +4,40 @@ import java.util.Scanner;
 
 public class ak {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		
 		Scanner s=new Scanner(System.in);
-		 int[] number=new int[5];
+		 int[] num=new int[5];
 
 		 System.out.println("enter the value");
+		 ak obj=new ak();
+		 //obj.method(number);
 		 
 		 for(int i=0;i<5;i++)
 		 {
-			 number[i]=s.nextInt();
+			 num[i]=s.nextInt();
 			 
-			 System.out.println(i);
-				 
-				 
-				 
-				 
-				 
-			  
+			// System.out.println(i);
 			 
-			 
-			 
-		 
-		 }	  
-		}
-		 
 		 }
+		 obj.method(num);
+		 for(int i =0;i<5;i++)
+		 {
+			 System.out.println(num[i]);
+		 }
+	}
+	
+int method(int num[])
+{
+	for (int i=0;i<5;i++)
+	{
+	num[i]=num[i]+3;
+	}
+	return 0;
+}		 
+		
+}
 		 
 		
 		 
