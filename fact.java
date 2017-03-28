627e9123a8e1a3c148579fa3c636b66f6b88d846
BTES17 <ramandeep@btes.co.in>
2017-03-28 22:09:26 +0530

    vishali(factorial)

--- fact.java
+++ fact.java
@@ -1,14 +1,26 @@
+package vishali;
+
 import java.util.Scanner;
-public class factorial {
-       
-    public static void main(String args[]){
-    int i,fact=1;
-   
-    Scanner sc = new Scanner(System.in);
-System.out.println("Enter a number to find factorial");  
-int n= sc.nextInt();
-    for(i=1;i<=n;i++){
-    fact=fact*i;
-    }
-    System.out.println("Factorial is: "+fact);
-}}
+
+public class fact {
+	
+		 public static void main(String args[])
+		 {  
+			 
+			 Scanner s=new Scanner(System.in);
+			 System.out.println("enter the number");
+			// a=s.nextLine();
+			 
+		  int i,fact=1;  
+		  int a = 1;
+		 // int number=5;//It is the number to calculate factorial    
+		  for(i=1;i<=a;i++)
+		  {    
+			  a=s.nextInt();
+		      fact=fact*i;    
+		  
+		  }    
+		  System.out.println("Factorial of "+a+" is: "+fact);    
+		 }  
+		}  
+
