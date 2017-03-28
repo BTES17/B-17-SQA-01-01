587f500f22e310ae7e8ce47dd8f9c67b1946a2c2
BTES17 <ramandeep@btes.co.in>
2017-03-28 22:06:53 +0530

    vishali

---
+++ sqr.java
@@ -0,0 +1,21 @@
+package vishali;
+
+import java.util.Scanner;
+
+ public class sqr
+ {
+	 public static void main(String args[])
+	 {
+ Scanner scan=new Scanner(System.in);
+ System.out.println("enter the values");
+ //int a=scan.nextInt();
+	 //int a=123456;
+ for(int i=1;i<=6;i++)
+ {
+	 int a=scan.nextInt();
+	int b=a*a;
+	System.out.println(b);
+ }
+	 }
+
+}
