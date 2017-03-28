76885762ae9354f8bb23d16eb189f3957b4377a0
BTES17 <ramandeep@btes.co.in>
2017-03-28 22:06:01 +0530

    jashan

---
+++ newarray.java
@@ -0,0 +1,17 @@
+
+
+public class newarray {
+
+	public static void main(String[] args) {
+		int[][]studentmarks;
+		studentmarks=new int[3][3];
+		
+	studentmarks[0][0]=22;
+	studentmarks[2][1]=23;
+	
+	System.out.println(studentmarks[0][0]);
+	System.out.println(studentmarks[1][2]);
+	System.out.println(studentmarks[2][1]);
+	
+	}
+}
