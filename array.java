295ad48c42ab31ebc28c4ba50b4511cf4af9243a
BTES17 <ramandeep@btes.co.in>
2017-03-28 23:51:36 +0530

    vinay(factorial)

--- array.java
+++ array.java
@@ -1,21 +1,18 @@
-package vinay;
+package sharma;
 
 public class array
 {
-	static array2 object=new array2();
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-int a=15,b=25;
-System.out.println(a+""+b);
-	object.display();
-	}
+		 int i, num=5,fact=1;
+		 for(i=1;i<=num;i++)
+		 {
+			 fact=fact*i;
+		 }
+		 System.out.print(fact);
+		 }
+
+	
 }
-class array2
-{
-	int a=45;
-		int display()
-		{
-			System.out.println(a);
-			return 0;
-		}
-	}
+	
+
