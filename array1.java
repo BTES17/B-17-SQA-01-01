90e66748d4c18cb0a4599ddf708660d25456922a
BTES17 <ramandeep@btes.co.in>
2017-03-28 11:41:50 +0530

    Harjeet (My array)

--- array1.java
+++ array1.java
@@ -1,21 +1,55 @@
 import java.util.Scanner;
 
-public class array1 
-{
-
-	public static void main(String[] args) 
+public class array1 {
+	public static void main(String[] args)
 	{
-		Scanner scan = new Scanner(System.in);
-		int a1[]= new int[10];
-		System.out.println("Enter the values");
-		for( int i=0;i<10;i++)
+		
+		array1 object=new array1();
+		System.out.println("Enter size of array");
+		Scanner scan=new Scanner (System.in);
+		int size=scan.nextInt();
+		int[] array=new int[size];
+		System.out.println("Enter all the elements");
+		
+		for(int i=0;i<size;i++)
 		{
-			a1[i]=scan.nextInt();
-		}		
-		for( int i=0;i<10;i++)
+			
+			array[i]=scan.nextInt();
+		}
+		
+		object.myarray(array,size);
+		
+		for(int i=0;i<size;i++)
 		{
-			System.out.println(a1[i]);
-		}		
+			
+			
+			System.out.println(array[i]);
+		}
+		
+		//System.out.println(array[4]);
+	}
+			
+	       int myarray(int b[],int size)
+			{
+	    	   
+				
+	    	   for(int i=0;i<size;i++)
+				{
+					
+	    		   b[i]=b[i]+3;
+	    		  System.out.println(b[i] + ",");
+				}
+				
+	    	   System.out.println("updated array");
+				
+	    	   return 0;
+			}
 	}
 
-}
+
+
+	
+	
+	
+
+
