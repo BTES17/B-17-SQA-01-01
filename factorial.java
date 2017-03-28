c52f6f4ef19754c1edcda8a1f3e0456da2f5783b
BTES17 <ramandeep@btes.co.in>
2017-03-28 22:18:29 +0530

    Anusha (factorial)

--- factorial.java
+++ factorial.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 
-
-public class factorial {
-
-	
-	public static void main(String[] args) {
-		 
-			 
-			  int i,fact=1;  
-			   Scanner scan=new Scanner(System.in);  
-			   System.out.println("Enter the number");
-			   int number=scan.nextInt();
-			   for(i=1;i<=number;i++)
-			   {    
-			      fact=fact*i;    
-			  }    
-			  System.out.println("Factorial of "+number+" is: "+fact);    
-			 }  
-			 
-
+public class factorial
+{
+	public static void main(String args[])
+	{
+		int n,fact = 1;
+		System.out.println("Print the no of which you want factorial");
+		Scanner scan= new Scanner(System.in);
+		n = scan.nextInt();
+		
+		for(int i=n;i>=1;i--)
+		{
+			fact= fact*i;
+			
+		}
+		System.out.println(fact);
 	}
-
-
+}
