f7500f91c87d754d9579048d826bea7e61341e0a
BTES17 <ramandeep@btes.co.in>
2017-03-28 08:46:21 +0530

    Ankit Rana

---
+++ class1.java
@@ -0,0 +1,16 @@
+public class Test {
+
+   public static void main(String args[])
+ {
+      int x = 30;
+
+      if( x < 30 ) 
+{
+         System.out.print("This is not a valid statement");
+      }
+else 
+{
+         System.out.print("This is valid statement");
+      }
+   }
+}
