8f2e5d19403a7eb9aff4a0cc5b3a65ec40feb0c0
BTES17 <ramandeep@btes.co.in>
2017-03-29 00:06:23 +0530

    vinay(factorialwhile)

---
+++ fabonicseries.java
@@ -0,0 +1,18 @@
+package sharma;
+
+public class fabonicseries 
+{
+
+	public static void main(String[] args) 
+	{
+		int fact=1,n=6;
+		while(n>=1)
+		{
+			fact=fact*n;
+			n--;
+		}
+             System.out.print(fact);		
+		}
+
+	}
+
