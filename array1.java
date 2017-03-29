8bfddabfc6acf702f3a8e77ea5ac7f5a0cfede45
BTES17 <ramandeep@btes.co.in>
2017-03-29 12:00:22 +0530

    Ankita 29 march

--- array1.java
+++ array1.java
@@ -1,55 +1,19 @@
+package bebo;
 import java.util.Scanner;
 
-public class array1 {
-	public static void main(String[] args)
+	public class array
 	{
-		
-		array1 object=new array1();
-		System.out.println("Enter size of array");
-		Scanner scan=new Scanner (System.in);
-		int size=scan.nextInt();
-		int[] array=new int[size];
-		System.out.println("Enter all the elements");
-		
-		for(int i=0;i<size;i++)
-		{
-			
-			array[i]=scan.nextInt();
-		}
-		
-		object.myarray(array,size);
-		
-		for(int i=0;i<size;i++)
-		{
-			
-			
-			System.out.println(array[i]);
-		}
-		
-		//System.out.println(array[4]);
-	}
-			
-	       int myarray(int b[],int size)
-			{
-	    	   
-				
-	    	   for(int i=0;i<size;i++)
-				{
-					
-	    		   b[i]=b[i]+3;
-	    		  System.out.println(b[i] + ",");
-				}
-				
-	    	   System.out.println("updated array");
-				
-	    	   return 0;
-			}
-	}
-
-
-
-	
-	
-	
-
+	   public static void main (String args[])
+	    {
+	        int[] a= new int[5];
+	        Scanner sc=new Scanner(System.in);
+	        System.out.println("Please enter numbers...");
+	        for(int j=0;j<5;j++)
+	        {
+	            a[j]=sc.nextInt();
+	        }
+	        for(int j=0;j<5;j++){
+	        System.out.println(a[j]);}
+	     }
+	} 
 
