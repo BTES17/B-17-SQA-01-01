45c18b9123f9a55478edc12a4b03eafa6780bef5
BTES17 <ramandeep@btes.co.in>
2017-03-29 16:13:38 +0530

    janky(series of odd no)

--- oddno.java
+++ oddno.java
@@ -1,21 +1,19 @@
-package vinay;
 import java.util.Scanner;
 public class oddno {
-
-	public static void main(String[] args)
-	{
-		int i;
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter the number");
-		int number=scan.nextInt();
-		  for( i=1;i<=number; i++)
-		  {
-			  if(i%2!=0)
-			  {
-				  System.out.print(i +" ");
-		
-
+	
+public static void main(String args[])
+		{
+			int a;
+			Scanner scan= new Scanner(System.in);
+			System.out.println("enter the no.");
+			a = scan.nextInt();
+			for(int i=1;i<=a;i++)
+			{
+				if(i%2 != 0)
+				{
+					System.out.print(i + " ");
+				}
+			}
+			
+		}
 	}
-		  }
-	}
-}
