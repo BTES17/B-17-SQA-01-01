7efb3347e7a34978cf9272ea79b34c21b1c96556
BTES17 <ramandeep@btes.co.in>
2017-03-29 08:39:39 +0530

    vishali (matrix)

---
+++ arrayrc.java
@@ -0,0 +1,27 @@
+package vishali;
+
+import java.util.Scanner;
+
+public class arrayrc
+{
+	public static void main(String[] args)
+{
+	Scanner scan=new Scanner(System.in);
+	System.out.println("enter the values");
+	
+		int[][] array=new int[5][6];
+		{
+			for(int c=1;c<=5;c++)
+			{
+				for(int r=1;r<=6;r++)
+				{
+				array[c][r]=scan.nextInt();
+				System.out.println(array[c][r]);
+				}
+			}
+		}
+		
+		
+}
+
+}
