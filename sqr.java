c731311de0c9c73f42ec501071bf274abc0358f5
BTES17 <ramandeep@btes.co.in>
2017-03-29 17:37:20 +0530

    vishali (sum of square numbers)

--- sqr.java
+++ sqr.java
@@ -9,13 +9,18 @@ import java.util.Scanner;
  Scanner scan=new Scanner(System.in);
  System.out.println("enter the values");
  int a=scan.nextInt();
-	
+ System.out.println("squaer of the number ");
+ int count=0;
+ 
+	 //int a=123456;
  for(int i=1;i<=a;i++)
  {
-
+	 
 	int b=i*i;
 	System.out.println(b);
+	count=count+b;
  }
+ System.out.println("sum of the series is" + count);
 	 }
 
 }
