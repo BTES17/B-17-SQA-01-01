b8ed7398f50fb2022e4f52dbd3250696ac78655b
BTES17 <ramandeep@btes.co.in>
2017-03-29 12:10:07 +0530

    shilpa
    factorial of 5

--- factorial.java
+++ factorial.java
@@ -1,19 +1,18 @@
+package sheena;
 
 public class factorial {
 
-	public static void main(String[] args) {
-		
-	//	Scanner factorial=new Scanner (System.in);
-		int i;
-		int fact=1;
-		int number=5;
-		
-		for(i=1;i<=5;i++)
-			fact=fact*i;
-		
-		System.out.println(fact);
-		
+	public factorial() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public static void main(String[] args) {
+	        int i,fact=1;
+	        int number=5;
+	        for(i=1;i<=5;i++){
+	        fact=fact*i;
+	        }
+	        System.out.println(fact);
 	}
 
 }
