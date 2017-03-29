d0274d05dd87d0671c204e11d03aba1b43278c60
BTES17 <ramandeep@btes.co.in>
2017-03-29 12:00:46 +0530

    sunil/29/03/17
    factorial

--- factorial.java
+++ factorial.java
@@ -1,19 +1,19 @@
-package soniyajoshi;
 
 public class factorial {
-	public static void main(String[] args)
-	{
 
-
-	int i=5;
-	int fact=1;
-	
-			
-			for(i=1;i<=5;i++)
-			
-	
-	         fact=fact*i;
+	public static void main(String[] args) {
+		
+	//	Scanner factorial=new Scanner (System.in);
+		int i;
+		int fact=1;
+		int number=5;
+		
+		for(i=1;i<=5;i++)
+			fact=fact*i;
+		
 		System.out.println(fact);
-	
-	}	
+		
+
+	}
+
 }
