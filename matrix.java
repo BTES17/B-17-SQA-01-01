25b669ebf234324dd224bc6778d91b11e7f62537
BTES17 <ramandeep@btes.co.in>
2017-03-29 11:55:31 +0530

    Sunil Kumar/29/03/17
    wap to print matrix

--- matrix.java
+++ matrix.java
@@ -1,23 +1,19 @@
- import java.util.Scanner;
-public class twoD {
-public static void main (String[] args) {
 
-Scanner scanner = new Scanner (System.in);
+public class matrix {
 
-System.out.println("Enter matrix 1: ");
-int [][] matrix = new int [4][3];
-for(int i=0;i<4;i++) 
-{
-for(int j=0;j<3;j++)
-{
-matrix [i][j] = scanner.nextInt();
-}
-for(i=0;i<4;i++)
-{
- for(int j=0;j<3;j++)
-{
-System.out.println(matrix[i][j]);
-}}
-}
-}
-} 
+	public static void main(String[] args) {
+		
+		int arr[][]={{1,2,7},{2,0,8},{6,6,9}};  
+		  
+		 
+		for(int i=0;i<3;i++){  
+		 for(int j=0;j<3;j++){  
+		   System.out.print(arr[i][j]+" ");  
+		 }  
+		 System.out.println();  
+		}  
+		  
+		}
+	}
+
+
