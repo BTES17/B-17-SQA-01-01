9d440550929977f0a9049729800c438363bf98f9
BTES17 <ramandeep@btes.co.in>
2017-03-29 11:59:20 +0530

    Ankit 29 march

--- array.java
+++ array.java
@@ -1,18 +1,19 @@
-package sharma;
+package bebo;
+import java.util.Scanner;
 
-public class array
-{
-	public static void main(String[] args)
+	public class array
 	{
-		 int i, num=5,fact=1;
-		 for(i=1;i<=num;i++)
-		 {
-			 fact=fact*i;
-		 }
-		 System.out.print(fact);
-		 }
-
-	
-}
-	
+	   public static void main (String args[])
+	    {
+	        int[] a= new int[5];
+	        Scanner sc=new Scanner(System.in);
+	        System.out.println("Please enter numbers...");
+	        for(int j=0;j<5;j++)
+	        {
+	            a[j]=sc.nextInt();
+	        }
+	        for(int j=0;j<5;j++){
+	        System.out.println(a[j]);}
+	     }
+	} 
 
