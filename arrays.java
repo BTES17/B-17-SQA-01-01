bebc742018278b3731a022d656001d3dcd82fd5e
BTES17 <ramandeep@btes.co.in>
2017-03-29 11:09:45 +0530

    Sonia Joshi 29/03/17
    WAP to print odd numbers.

---
+++ arrays.java
@@ -0,0 +1,16 @@
+package soniyajoshi;
+
+public class arrays 
+{
+	public static void main(String[] args)
+	{
+		int i;
+  for(i=1;i<=10;i++)
+  {
+	  if(i%2!=0)
+		  System.out.println(i);
+		  
+  }
+	
+}
+}
