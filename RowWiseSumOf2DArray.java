efa73e7d2a7fb6b721300976e7f117c8ba397502
BTES17 <ramandeep@btes.co.in>
2017-03-30 13:12:52 +0530

    Anusha 30/03/2017 ( Row wise sum of 2-d array)

---
+++ RowWiseSumOf2DArray.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public abstract class RowWiseSumOf2DArray 
+{
+	public static void main(String args[])
+	{
+		int row,column;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the no. of rows");
+		row = scan.nextInt();
+		System.out.println("Enter the no. of columns");
+		column = scan.nextInt();
+		int a[][] = new int[row][column];
+		System.out.println("Enter the elements for" + row + "x" +column + "array");
+		for(int i=0;i<row;i++)
+		{
+			for(int j=0;j<column;j++)
+			{
+				a[i][j] = scan.nextInt();
+			}
+		}
+		System.out.println("The matrix is:");
+		for(int i=0;i<row;i++)
+		{
+			for(int j=0;j<column;j++)
+			{
+				System.out.print(a[i][j] + " " );
+			}
+			System.out.println();
+		}
+		System.out.println("The sum of rows is:");
+		int count=0;
+		for(int i=0;i<row;i++)
+		{
+			for(int j=0;j<column;j++)
+			{
+				count = count + a[i][j]; 
+				
+				
+			}
+			System.out.print(count + " " );
+
+			System.out.println();
+			count=0;
+		}	
+	}
+}
