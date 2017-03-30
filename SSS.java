3199a4afbb427cf89831ac21fd99bddcbb5c03ef
BTES17 <ramandeep@btes.co.in>
2017-03-30 21:14:12 +0530

    sunil/29/03/17
    Print the fibnaci series

---
+++ SSS.java
@@ -0,0 +1,27 @@
+
+public class SSS {
+
+	public static void main(String[] args) {
+
+
+			int a = 0;
+			int b = 1;
+			int c = 0;
+
+			System.out.print( a + ", " + b );
+
+			for ( int i = b; i < 5; ++i )
+			{
+			c = a + b;
+			System.out.print(", " + c );
+			a = b;
+			b = c;
+
+			}
+			}
+
+			
+
+	}
+
+
