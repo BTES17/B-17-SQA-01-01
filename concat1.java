1f5c7eb631c7046d1478bd6ef9a4422d700415d1
BTES17 <ramandeep@btes.co.in>
2017-03-30 21:47:06 +0530

    Ankit 30 march

---
+++ concat1.java
@@ -0,0 +1,13 @@
+package bebo;
+
+public class string1 {
+	public static void main(String args[])
+	{
+		String s1= "Sachin";
+		String s2="SACHIN";
+		String varString= new String("Sachin");
+		String varString1= new String("SACHIN");
+		System.out.println(s1.concat(s2));
+	}
+
+}
