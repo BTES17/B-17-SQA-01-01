34f68202e73a3761eabab6dbb73f606cd47a31ca
BTES17 <ramandeep@btes.co.in>
2017-04-03 12:15:22 +0530

    sunil....
    reverse a string

---
+++ sk.java
@@ -0,0 +1,20 @@
+package pkg1;
+
+public class sk {
+
+	public static void main(String[] args) {
+		  
+		   String s="java";
+		int size=s.length();
+		System.out.println(size);
+		int i;
+		for(i=size-1; i>=0; i--)
+		{
+		System.out.println(s.charAt(i));
+
+		
+		
+
+	}
+	}}
+
