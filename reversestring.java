4fbaf152966a1232c688999bbf39ee1120476841
BTES17 <ramandeep@btes.co.in>
2017-04-03 22:57:12 +0530

    Ankita 3 april

---
+++ reversestring.java
@@ -0,0 +1,21 @@
+package bebo;
+
+public class Ab {
+	
+
+		public static void main(String[] args) {
+			  
+			   String s="java";
+			int size=s.length();
+			System.out.println(size);
+			int i;
+			for(i=size-1; i<=0; i++)
+			{
+			System.out.println(s.charAt(i));
+
+			
+			
+
+		}
+		}}
+
