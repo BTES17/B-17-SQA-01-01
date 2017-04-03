784120758e72530e6d246f5e22d8fca1804a2dea
BTES17 <ramandeep@btes.co.in>
2017-04-03 11:56:03 +0530

    Deepshi (3-4-17)

---
+++ Reverse_String.java
@@ -0,0 +1,16 @@
+package d;
+
+public class ReverseString {
+	public static void main(String args[])
+	{
+		String str=new String ();
+		str=("Hello java");
+		int size=str.length();
+		for(int i=size-1;i>=0;i--)
+		{
+			System.out.print(str.charAt(i));
+		}
+	
+	}
+
+}
