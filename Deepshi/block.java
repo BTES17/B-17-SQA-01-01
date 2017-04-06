b9722982accea616aee25e02b499eabb3987e0c1
BTES17 <ramandeep@btes.co.in>
2017-04-06 11:10:49 +0530

    block

---
+++ Deepshi/block.java
@@ -0,0 +1,21 @@
+public class Demo {
+public static void main(String args[])
+{
+	System.out.println("hello");
+	Demo a1 = new Demo();
+	a1.block();
+	}
+void block()
+{
+	for(int i=0;i<5;i++)
+	{
+		System.out.println(i);
+	}
+}
+{
+	int a=9;
+	System.out.println(a);
+	int b=a+2;
+	System.out.println(b);
+}
+}
