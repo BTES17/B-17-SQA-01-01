14d81f3bfadc89dc9e245bda19c7d358034ababf
BTES17 <ramandeep@btes.co.in>
2017-04-06 11:27:01 +0530

    singleBlock

---
+++ Ravdeep/vel.java
@@ -0,0 +1,21 @@
+
+public class vel {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		vel a1 = new vel();
+		a1.demo();
+	}
+	void demo()
+	{
+		
+	}
+	 {
+		int a=9;
+		System.out.println(a);
+		int b=a++;
+		System.out.println(b);
+	}
+	
+}
