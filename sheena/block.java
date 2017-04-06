05a0f70269a32a84504147e98975ed828dabaf2d
BTES17 <ramandeep@btes.co.in>
2017-04-06 11:02:08 +0530

    block

---
+++ sheena/block.java
@@ -0,0 +1,29 @@
+package sheena;
+
+public class block {
+public static void main(String args[])
+{
+	block s= new block();
+
+	System.out.println("hii");
+
+s.method();}
+	void method()
+	{int i;
+		for(i=0;i<=5;i++)
+		{
+			System.out.println(i);
+			
+		}
+		
+	}
+
+{
+	int a=11;
+	
+System.out.println(a);
+	int b=a+2;
+	System.out.println(b);
+	}
+
+}
