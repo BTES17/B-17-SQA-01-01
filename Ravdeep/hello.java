e2bee63dfeaee92d6baa8765d0ed729d120d8fa4
BTES17 <ramandeep@btes.co.in>
2017-04-06 11:29:10 +0530

    staticImport

---
+++ Ravdeep/hello.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+public class hello {
+public static void main(String args[])
+{
+	System.out.println("Hi");
+}
+void abc()
+{
+	
+}
+static{
+	int a=5;
+	out.println(a);
+}
+}
