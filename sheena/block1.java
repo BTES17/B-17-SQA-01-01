5441a297816c7f0455d1966c5c347e74991dc84d
BTES17 <ramandeep@btes.co.in>
2017-04-06 11:24:41 +0530

    block1

---
+++ sheena/block1.java
@@ -0,0 +1,21 @@
+package sheena;
+
+public class block1 {
+	public static void main(String args[])
+	{
+		block s= new block();
+
+		System.out.println("hii");
+
+	s.method();}
+
+		void method()
+		{int i;
+			for(i=0;i<=5;i++)
+			{
+				System.out.println(i);
+				
+			}
+		}
+			
+}
