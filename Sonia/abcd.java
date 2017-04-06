511acd01e52689136e8e866bbded635042c70665
BTES17 <ramandeep@btes.co.in>
2017-04-06 12:17:42 +0530

    06/04/17
    WAP to enter details of 5 students,displaying their record and calculating percentage of each student.

--- Sonia/abcd.java
+++ Sonia/abcd.java
@@ -9,7 +9,7 @@ public class abcd {
 		int rollno; 
 		float marks;
 		String name;
-		static String clgname; 
+		static String clgname="IET"; 
 			
 	public static void main(String[] args) 
 	{
@@ -20,6 +20,10 @@ public class abcd {
 	    
 			
 			abcd obj=new abcd();
+			abcd obj1=new abcd();
+			abcd obj2=new abcd();
+			abcd obj3=new abcd();
+			abcd obj4=new abcd();
 			
 			
 		    obj.add();
@@ -27,6 +31,22 @@ public class abcd {
 			obj.percentage();
 			
 				
+			    obj1.add();
+				obj1.display();
+				obj1.percentage();
+				
+				    obj2.add();
+					obj2.display();
+					obj2.percentage();
+					
+					    obj3.add();
+						obj3.display();
+						obj3.percentage();
+						
+						    obj4.add();
+							obj4.display();
+							obj4.percentage();
+							
 				
 		}	
 	
@@ -42,7 +62,7 @@ public class abcd {
 			System.out.println("Enter marks of student");
 			  marks=scan.nextInt();
 			System.out.println("Enter clgname of student");
-			  clgname=scan.next();
+			 
 	 }
 		
 		
@@ -52,7 +72,7 @@ public class abcd {
 			System.out.println(rollno);
 			System.out.println(marks);
 			System.out.println(clgname);
-			System.out.println(totalmarks);
+			
 	
 			
 		}
@@ -65,7 +85,8 @@ public class abcd {
 	
 			 
 		 }
-		static {
+		
+        static {
 			 int a=10;
 			 int b=30;
 			 int c=a+b;
@@ -73,12 +94,13 @@ public class abcd {
 			 
 			 
 		 }
-		 {
+		static {
 			 int a=110;
 			 int b=30;
 			 int c=a+b;
 			 System.out.println(c);
 			 
 		}
-		
 }
+		
+
