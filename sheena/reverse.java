67d6ca3025e0618f421b1c62f7299be6c37cb79b
BTES17 <ramandeep@btes.co.in>
2017-04-06 12:14:20 +0530

    ReverseString

---
+++ sheena/reverse.java
@@ -0,0 +1,17 @@
+
+public class reverse {
+	
+		public static void main(String args[])
+		{
+			String str=new String ();
+			str=("Hello java");
+			int size=str.length();
+			for(int i=size-1;i>=0;i--)
+			{
+				System.out.print(str.charAt(i));
+			}
+		
+		}
+
+	}
+
