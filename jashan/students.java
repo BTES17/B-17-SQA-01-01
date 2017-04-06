5ce17731ddb890334fc8afbcea7201c790efa814
BTES17 <ramandeep@btes.co.in>
2017-04-06 11:09:19 +0530

    constructer

--- jashan/students.java
+++ jashan/students.java
@@ -1,70 +1,61 @@
+package jashan;
+
 import java.util.Scanner;
 
-public class students 
-{
+public class students {
 	String name;
-	int rollno;
-	float marks;
-	static String colg_name;
-	 int a,b;
+	int roll_no;
+	int marks;
+	static String clg_name;
 	
-		public static void main(String args[])
-		{
-			
-			students obj = new students();
-			obj.clg();
-			students obj1 = new students();
-			obj1.add();
-			obj1.display();
-			obj1.percentage();
-			students obj2 = new students();
-			obj2.add();
-			obj2.display();
-			obj2.percentage();
-			students obj3 = new students();
-			obj3.add();
-			obj3.display();
-			obj3.percentage();
-			students obj4 = new students();
-			obj4.add();
-			obj4.display();
-			obj4.percentage();
-			students obj5 = new students();
-			obj5.add();
-			obj5.display();
-			obj5.percentage();
-			
+public static void main(String []args)
 
-		}
-		void clg()
-		{
-			System.out.println("Enter the name of college");
-			Scanner scan= new Scanner(System.in);
-			colg_name= scan.next();
-		}
-		void add()
-		{  
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Enter the name");
-			name = scan.next();
-			System.out.println("Enter the rollno");
-			rollno = scan.nextInt();
-			System.out.println("Enter the marks");
-			marks = scan.nextInt();
-			
-		}
-		void display()
-		{
-			System.out.println(name);
-			System.out.println(rollno);
-			System.out.println(marks);
-			System.out.println(colg_name);
-		}
-		void percentage()
-		{
-			float percent = marks/5;
-			System.out.println(percent + "" + "%");
-		}
+{
+
+		students obj=new students("btes");
+		//obj.add();
+		students obj1=new students("jashan",20,30 );
+		//obj1.add();
+		obj1.display();
+		obj1.percentage();
+		students obj2=new students("abc",40,50);
+		//obj2.add();
+		obj2.display();
+		obj2.percentage();
+		students obj3=new students("abd",10,20);
+		//obj3.add();
+		obj3.display();
+		obj3.percentage();
 		
-	}
-
+	
+}
+		students  (String str2 ){
+			//Scanner scan=new Scanner(System.in);
+			//System.out.println("enter the name");
+			 //name=scan.next();
+			 //System.out.println("enter the roll_no");
+			 //roll_no=scan.nextInt();
+			 //System.out.println("enter the marks");
+			 //marks=scan.nextInt();
+			// System.out.println("enter the clg_name");
+			 clg_name=str2;
+			 
+		}students  (String str,int i,int j){
+			name=str;
+			roll_no=i;
+			marks=j;
+		}
+		void display(){
+			System.out.println(name);
+			System.out.println(roll_no);
+			System.out.println(marks);
+			System.out.println(clg_name);
+		}
+		void percentage(){
+			float percentage=marks/5;
+			System.out.println(percentage +" "+ "%");
+			
+			
+			
+		}
+}
