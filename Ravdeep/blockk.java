9c039a56313c82fd4c2c7302c4406b315618156f
BTES17 <ramandeep@btes.co.in>
2017-04-06 11:00:53 +0530

    Block

---
+++ Ravdeep/blockk.java
@@ -0,0 +1,22 @@
+
+public class blockk {
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		blockk a1 = new blockk();
+		a1.demo();
+	}
+	void demo()
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(i);
+		}
+	}
+	{
+		int a=9;
+		System.out.println(a);
+		int b=a+2;
+		System.out.println(b);
+	}
+}
