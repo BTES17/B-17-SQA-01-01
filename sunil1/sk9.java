ce35ae28efff6a814932e9d8602de6238f813705
BTES17 <ramandeep@btes.co.in>
2017-04-06 11:25:13 +0530

    blocks /static block

---
+++ sunil1/sk9.java
@@ -0,0 +1,35 @@
+package soniyajoshi;
+
+public class sk9 {
+
+	public static void main(String[]args){
+		int a=20;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+		}
+	
+	
+	{
+	int a=20;
+	int b=10;
+	int c=a+b;
+	System.out.println(c);
+	}
+	
+	
+  static {
+  int a=20;
+  int b=40;
+ int c=a+b;
+   
+ System.out.println(c);
+  }
+static {
+	  int a=20;
+	  int b=70;
+	 int c=a+b;
+	   
+	 System.out.println(c);
+	  }}
+
