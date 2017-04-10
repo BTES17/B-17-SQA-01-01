ae27a4e3f25db6e6191c59da7c68d371690a2b03
BTES17 <ramandeep@btes.co.in>
2017-04-10 10:41:41 +0530

    vinay(reverse array)

---
+++ Vinay/reverseee.java
@@ -0,0 +1,21 @@
+package vinay;
+
+public class reverseee {
+public static void main(String args[])
+{
+	String str=new String();
+	str="Hello Java";
+	
+
+	int size=str.length();
+	for(int i=size-1;i>=0;i--)
+	{  
+		System.out.print(str.charAt(i));
+	}
+
+		System.out.print(str.toUpperCase());
+	}
+	
+	
+}
+
