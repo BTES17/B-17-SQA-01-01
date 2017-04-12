fdf439764b206c54efa38aad26882a21a0711c24
BTES17 <ramandeep@btes.co.in>
2017-04-12 22:25:32 +0530

    different data type, same no. of parameters

---
+++ Inderpal/poly2.java
@@ -0,0 +1,20 @@
+package Inderpal;
+public class same {
+  public static void main(String args[])
+  {
+    same obj=new same();
+    obj.show(7,6);
+    obj.show(9,3);
+  
+  }
+  void show(int a, float b)
+  {
+      System.out.println(a);
+      System.out.println(b);
+  }
+  void show(float a,double b)
+  {
+      System.out.println(a);
+      System.out.println(b);
+  }
+}
