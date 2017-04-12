0e0f83093b04d97f159785ea8e20d2425510054f
BTES17 <ramandeep@btes.co.in>
2017-04-12 23:22:15 +0530

    same no of arguements, different data types

---
+++ polymorphism2.java
@@ -0,0 +1,23 @@
+
+package Ravdeep;
+
+public class poly2 {
+    
+  void show(int a, float b)
+  {
+      System.out.println(a);
+      System.out.println(b);
+  }
+  void show(float a,float b)
+  {
+      System.out.println(a);
+      System.out.println(b);
+  }
+  public static void main(String args[])
+  {
+    poly2 obj=new poly2();
+    obj.show(2,3);
+    poly2 obj1=new poly2();
+    obj1.show(4,5);
+  }
+}
