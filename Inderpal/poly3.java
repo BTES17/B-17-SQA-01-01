4032d89147b0e74cda34c02d8ff0a1dc16a3a1e0
BTES17 <ramandeep@btes.co.in>
2017-04-12 22:32:12 +0530

    same data type, no. of arguements, different order

---
+++ Inderpal/poly3.java
@@ -0,0 +1,21 @@
+package Inderpal;
+public class formal {
+    {
+    formal obj=new formal();
+    obj.show(7,6,8);
+    obj.show(4,3,5);
+  
+  }
+  void show(int a,int b,int c)
+  {
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+  }
+  void show(int b,int c,int a)
+  {
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(a);
+  }
+}
