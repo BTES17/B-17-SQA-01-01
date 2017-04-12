b73cf3ade8fc89b0b5b72259ae0eb4e704ddcf3e
BTES17 <ramandeep@btes.co.in>
2017-04-12 22:36:17 +0530

    final on variable

---
+++ Inderpal/finalvariable.java
@@ -0,0 +1,14 @@
+package Inderpal;
+public class fii {
+   
+  public static void main(String args[])
+  {
+      final int a=7;
+      System.out.println(a);
+  }
+  void abc()
+  {
+   int a=5;
+   System.out.println(a);
+  }
+}
