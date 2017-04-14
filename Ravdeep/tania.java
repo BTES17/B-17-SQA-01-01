a04a373b8fc05872617ddd0b49fb3ff23db4c4aa
BTES17 <ramandeep@btes.co.in>
2017-04-14 11:52:49 +0530

    static var & function in Interface

---
+++ Ravdeep/tania.java
@@ -0,0 +1,25 @@
+package Ravdeep;
+
+public interface tania {
+abstract void xyz();
+static int a=6;
+public static void main(String args[])
+{
+	Raina obj=new Raina();
+	obj.deep();
+	obj.xyz();
+}}
+	class Raina implements tania{
+		
+		static void deep()
+		{
+
+	int b=55;
+	int c=a+b;
+	System.out.println(c);
+}
+public void xyz()
+{
+	System.out.println("Hello");
+}
+}
