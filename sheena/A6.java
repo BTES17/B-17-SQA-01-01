238675c88411c453baccf22760e9cdd2ed04862b
BTES17 <ramandeep@btes.co.in>
2017-04-14 10:54:10 +0530

    INTERFACE

---
+++ sheena/A6.java
@@ -0,0 +1,15 @@
+package smayra1;
+
+   interface print{  
+    void print();  
+    }  
+    class A6 implements print{  
+    public void print(){System.out.println("Hello");}  
+      
+    public static void main(String args[]){  
+    A6 obj = new A6();  
+    obj.print();  
+     }  
+    }  
+
+
