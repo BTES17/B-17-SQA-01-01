4374be44e24b5c22b1138934a8efd27fa30b07ef
BTES17 <ramandeep@btes.co.in>
2017-04-15 23:49:15 +0530

    abstract1

---
+++ Aa/ankita.java
@@ -0,0 +1,15 @@
+package bebo;
+
+public abstract class ankita {	
+	public static void main(String args[])
+	{
+		
+	}
+	void abc()
+	{
+		int a=2,b=4,c;
+		c=a+b;
+		System.out.println(c);
+	}
+	abstract void xyz();
+}
