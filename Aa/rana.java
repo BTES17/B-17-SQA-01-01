838ec65510c69a8d6d802e01bc916e5e745c6ac3
BTES17 <ramandeep@btes.co.in>
2017-04-15 23:49:57 +0530

    abstract2

---
+++ Aa/rana.java
@@ -0,0 +1,15 @@
+package bebo;
+
+public class rana extends ankita {
+	public static void main(String args[])
+
+		{
+			rana obj=new rana();
+			obj.abc();
+			obj.xyz();
+		}
+	void xyz()
+	{
+		System.out.println("hello");
+	}
+	}
