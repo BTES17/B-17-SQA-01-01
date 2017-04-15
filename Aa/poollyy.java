e92331c06fd9b26f11b46bbc619aa1ee90596762
BTES17 <ramandeep@btes.co.in>
2017-04-15 23:03:44 +0530

    Add files via upload

---
+++ Aa/poollyy.java
@@ -0,0 +1,19 @@
+package bebo;
+
+public class poollyy {
+		public static void main(String args[])
+		{
+			
+			    poollyy obj=new poollyy();
+			    obj.show(5,6);
+			            obj.show(9);
+			  }
+			  void show(int a)
+			  {
+			      System.out.println(a);
+			  }
+			  void show(float a,int b)
+			  {
+		
+			  }
+	}
