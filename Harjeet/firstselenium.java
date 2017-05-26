888ba3a81697765b15818e678aa513623179df12
BTES17 <ramandeep@btes.co.in>
2017-05-26 12:23:21 +0530

    fs1

---
+++ Harjeet/firstselenium.java
@@ -0,0 +1,82 @@
+package selenium;
+import POI.Hls_Reader;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Navigation;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstselenium {
+	
+		static Hls_Reader obj1=new Hls_Reader("C:\\Documents and Settings\\install\\Desktop\\seleniumfirst.xls");
+	
+	public static void main(String[] args) {
+		
+		
+
+			
+			String url=System.getProperty("user.dir")+"\\src\\chromedriver.exe";
+			 
+			System.setProperty("webdriver.chrome.driver",url);
+			System.out.println(System.getProperty("webdriver.chrome.driver"));
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.facebook.com/");
+			//driver.get("https://172.16.0.42/placementcell/admin/");
+			String title=driver.getTitle();
+			System.out.println(title);
+			//String pagesource=driver.getPageSource();
+			//System.out.println(pagesource);
+			//driver.close();
+			//driver.quit();
+			WebElement Un_element=driver.findElement(By.id("email"));
+			String UN=obj1.getCellData("Sheet1","User_name",2);
+					Un_element.sendKeys(UN); 
+					
+					 
+					WebElement Pw_element=driver.findElement(By.name("pass"));
+					String PS=obj1.getCellData("Sheet1","Password",2);
+		     Pw_element.sendKeys(PS);
+		     
+		     WebElement Login=driver.findElement(By.id("u_0_q"));
+		     Login.click();
+		     
+		 	String ER=obj1.getCellData("Sheet1","Expected_result",2);
+		  
+		    //   driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		   /* try 
+		     {
+		    Thread.sleep(4000);			
+		     }
+		    		catch(Exception n)
+		    		{
+		    		System.out.println(n);
+		    	}*/
+		     System.out.println("endleep");
+		     WebElement o=driver.findElement(By.linkText("Games"));
+		     o.click();
+		     
+		     
+		            
+		    /* try 
+		     {
+		    	Thread.sleep(4000);
+		     }
+		    		catch(Exception n)
+		    		{
+		    		System.out.println(n);
+		    	}*/
+		    // System.out.println("endleep");
+		    	
+		     
+		     
+		    // Navigation obj=driver.navigate();
+		   //  driver.navigate().back();
+		   //  driver.navigate().forward();
+		  //   driver.navigate().refresh();
+		   //  driver.navigate().to("https://www.google.com/");
+			
+			}
+	}
+
+
