f7b0e8664124fc950a52708b493b97e7a048dd7c
BTES17 <ramandeep@btes.co.in>
2017-05-31 12:28:38 +0530

    Running testcases

--- Ravdeep/Selenium2.java
+++ Ravdeep/Selenium2.java
@@ -1,5 +1,5 @@
-package sj;
 
+package jashan;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.concurrent.TimeUnit;
@@ -9,23 +9,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import POI.Hls_Reader;
 
 
 
 public class Selenium2 {
-	Hls_Reader obj=new Hls_Reader("C:\\rav.xls");
+	static Hls_Reader obj=new Hls_Reader("C:\\soniya.xls");
 	static WebDriver driver;
 	//static WebDriver driver1;
-	setup(int testcase)
+	static void setup(int testcase)
 	{
-		if(getCellData.obj("Sheet1", "Browser", testcase).equals("Google Chrome"))
+		if(obj.getCellData("Sheet1", "Browser", testcase).equals("Google Chrome"))
 		{
 			String directory = "D:\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", directory);
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		}
-		else if(getCellData.obj("Sheet1", "Browser", testcase).equals("Mozilla"))
+		else if(obj.getCellData("Sheet1", "Browser", testcase).equals("Mozilla"))
 		{
 			driver =new FirefoxDriver();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -33,7 +37,7 @@ public class Selenium2 {
 		}
 		
 	}
-	testcase(int testcase)
+	static void testcase(int testcase)
 	{
 		WebElement un_get = driver.findElement(By.id("email"));
 		WebElement pwd = driver.findElement(By.id("pass"));
@@ -43,31 +47,45 @@ public class Selenium2 {
 		String pw=obj.getCellData("Sheet1", "Password", testcase);
 		pwd.sendKeys(pw);
 		System.out.println(pw);
-		WebElement click=driver.findElement(By.id("u_0_q"));
+		WebElement Login=driver.findElement(By.id("u_0_q"));
 		//WebElement un_get1=driver.findElement(By.name("notifications"));
-		click.click();
+		Login.click();
 	}
-	Result(int testcase)
+	static void Result(int testcase)
 	{
-		
+		String expected_result= obj.getCellData("Sheet1","Expected Result",testcase);
+				WebElement e= (new WebDriverWait(driver, 30)).until(ExpectedConditions.presenceOfElementLocated(By.id("email")));
+				String Actual_Result= driver.getCurrentUrl();
+				
+		if(expected_result.equals(Actual_Result))
+		{
+			 obj.setCellData("Sheet1","Actual Result",testcase,"Pass");
+		}
+		else
+		{
+			obj.setCellData("Sheet1","Actual Result",testcase,"Fail");
+		}
+		driver.close();
 	}
 	public static void main(String args[])
-	{ int row=obj.getrowcount();
-		for(int i=0;i<=row;i++)
+	{ int row=obj.getRowCount("Sheet1");
+		for(int i=2;i<=row;i++)
 		{
-			if(getCelldata("Sheet1", "Username", 2).equals(ON))
+			if(obj.getCellData("Sheet1", "Status", row).equals("ON"))
 			{
 				setup(i);
 				testcase(i);
 				Result(i);
 			}
+		
 		}
+		
 	
-		String directory = "D:\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver", directory);
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get("https://www.facebook.com/");
+		//String directory = "D:\\chromedriver.exe";
+		//System.setProperty("webdriver.chrome.driver", directory);
+		//driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		//driver.get("https://www.facebook.com/");
 		//driver.get("http://www.facebook.com");
 		//String title = driver.getTitle();
 		//System.out.println(title);
@@ -82,9 +100,7 @@ public class Selenium2 {
 		pwd1.sendKeys(id);
 		String ps=obj.getCellData("sheet1", "password", 4);
 		WebElement click1=driver.findElement(By.id("u_0_q"));
-
 		click.click();*/
 	}
 }
-
 
